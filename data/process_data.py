c762b5837e56fec305339daff7901e36ec898caa
Charles Modingwa <34737460+Modingwa@users.noreply.github.com>
2019-08-09 22:23:19 +0200

    Update process_data.py script
    Added docstrings to the functions and minor changes to the load data function.

--- data/process_data.py
+++ data/process_data.py
@@ -6,16 +6,39 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """Function to load messages and categories datasets
+
+    Parameters
+    ----------
+    messages_filepath : str
+        Path to messages csv file
+	categories_filepath: str
+        Path to categories csv file
+
+    Returns
+    -------
+    Pandas DataFrame
+        The dataframe is composed of the messages and categories datasets joined on id.
+    """
     # load messages dataset
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     # merge datasets
-    df = messages.merge(categories, how='inner')
+    df = messages.merge(categories, how='inner', on='id')
 
     return df
 
-
 def clean_data(df):
+    """Function to clean the Pandas DataFrame created from the load_data function
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Messages and Categories Pandas DataFrame
+    -------
+    Pandas DataFrame
+        Returns a clean Pandas DataFrame
+    """
     # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(';', expand=True)
     # select the first row of the categories dataframe
@@ -44,12 +67,30 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """Function to save the clean dataframe to sqlite database
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        clean Pandas DataFrame
+	database_filename: str
+        Path to sqlite database destination file
+
+    Returns
+    -------
+    None
+    """
     engine = create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('messages', engine, index=False)
 
-
-
 def main():
+    """Main function to run the ETL pipeline
+
+    Functions:
+    1) Extraction -> extract messages and categories from csv files
+	2) Transformation -> clean and pre-process data
+	3) Load -> load the clean dataframe to SQLite local database
+	"""
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
